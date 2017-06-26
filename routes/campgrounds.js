112f1e4cf2c53bea45f4d3bd2e202fcf0a58fdf7
raydabbah <dabbahray@yahoo.com>
2017-06-26 11:48:34 -0400

    added users to campgrounds-it works

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -5,7 +5,7 @@ const campground = require('../models/campgrounds');
 router.get("/", (req, res) => {
 	campground.find({}, function (err, allCampgrounds) {
 		if (err) {
-			 console.log(err);
+			console.log(err);
 		} else {
 			res.render("campgrounds", {
 				campgrounds: allCampgrounds
@@ -18,16 +18,24 @@ router.post("/", isLoggedIn, (req, res) => {
 	var name = req.body.name;
 	var image = req.body.image;
 	var desc = req.body.description;
+	var user = req.user.username;
+	var userId = req.user._id;
+	console.log(req.user)
 	var newCampGround = {
 		name: name,
 		image: image,
-		description: desc
+		description: desc,
+		author: {
+			username: user,
+			id: userId,
+		}
 	};
 	campground.create(newCampGround, function (err, latestCampground) {
 		if (err) {
 			res.redirect("/campgrounds/new");
-			 console.log('There was en error adding your campground.');
+			console.log('There was en error adding your campground.');
 		} else {
+			console.log(latestCampground)
 			res.redirect("/campgrounds");
 		}
 	});
@@ -36,7 +44,7 @@ router.get("/new", isLoggedIn, (req, res) => res.render("newCampground"));
 router.get("/:id", function (req, res) {
 	campground.findById(req.params.id).populate("comments").exec(function (err, clickedOnCg) {
 		if (err) {
-			 console.log(err);
+			console.log(err);
 		} else {
 			res.render("show", {
 				campground: clickedOnCg
