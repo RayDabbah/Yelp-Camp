f8288ae1c909dd07ffe10857755938b541aafa6f
raydabbah <dabbahray@yahoo.com>
2017-06-25 21:37:24 -0400

    add isLoggedIn function

--- app.js
+++ app.js
@@ -11,15 +11,15 @@ var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({
 	extended: true
 }));
-
-app.use(express.static("public"));
-app.set("view engine", "ejs");
-
 app.use(require("express-session")({
 	secret: 'These are THE best campgrounds the world has to offer!!!!!',
 	resave: false,
 	saveUninitialized: false,
 }));
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
@@ -45,7 +45,7 @@ app.get("/campgrounds", (req, res) => {
 	});
 
 });
-app.post("/campgrounds", (req, res) => {
+app.post("/campgrounds", isLoggedIn, (req, res) => {
 	var name = req.body.name;
 	var image = req.body.image;
 	var desc = req.body.description;
@@ -63,7 +63,7 @@ app.post("/campgrounds", (req, res) => {
 		}
 	});
 });
-app.get("/campgrounds/new", (req, res) => res.render("newCampground"));
+app.get("/campgrounds/new", isLoggedIn, (req, res) => res.render("newCampground"));
 app.get("/campgrounds/:id", function (req, res) {
 	campground.findById(req.params.id).populate("comments").exec(function (err, clickedOnCg) {
 		if (err) {
@@ -76,7 +76,7 @@ app.get("/campgrounds/:id", function (req, res) {
 		}
 	});
 });
-app.get("/campgrounds/:id/comments/new", (req, res) => {
+app.get("/campgrounds/:id/comments/new", isLoggedIn, (req, res) => {
 	campground.findById(req.params.id, (err, campground) => {
 		if (err) {
 			console.log(err);
@@ -89,7 +89,7 @@ app.get("/campgrounds/:id/comments/new", (req, res) => {
 		}
 	});
 });
-app.post("/campgrounds/:id/comments", (req, res) => {
+app.post("/campgrounds/:id/comments", isLoggedIn, (req, res) => {
 	campground.findById(req.params.id, (err, campground) => {
 		Comment.create(req.body.comment, (err, comment) => {
 			if (err) {
@@ -135,7 +135,14 @@ app.post("/login", passport.authenticate("local", {
 app.get("/logout", (req, res)=> {
 	req.logout();
 	res.redirect("/campgrounds");
-});
+}); 
+
+function isLoggedIn(req,res, next) {
+	if(req.isAuthenticated()){
+		return next();
+	}
+	res.redirect("/login");
+}
 
 app.get("*", (req, res) => res.redirect("/"));
 
